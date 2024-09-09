88390897e5565af53b1d070919305d543621a286
Carlosgabriel62 <124841314+Carlosgabriel62@users.noreply.github.com>
2024-09-09 10:03:16 -0300

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-//Iago Leite 10418120, Carlos Gabriel 10410489
-
-public class Main {
-  public static void main(String[] args) {
-    GerenciadorNomes gNomes = new GerenciadorNomesMem();
-    Ihm ihm = new Ihm(gNomes);
-    ihm.dialogar();
-  }
-}
